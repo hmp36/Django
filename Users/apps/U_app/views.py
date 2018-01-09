26aa45db2dc30888662c597ee5281e3406c24753
Hagan <haganpratt@gmail.com>
2018-01-09 09:06:58 -0500

    and

---
+++ Users/apps/U_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,"U_app/index.html")
+   
+
+
+# Create your views here.
---
+++ Users/apps/dojo_ninja_app/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc= models.CharField(max_length=500)
+
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
